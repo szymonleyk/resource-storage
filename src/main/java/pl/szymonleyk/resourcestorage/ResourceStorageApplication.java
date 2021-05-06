87540781413eb79914e31d1771325bf0f83880b7
Szymon Leyk <biuro@szymonleyk.pl>
2021-05-06 14:20:44 +0200

    #1 wygenerowanie projektu startowego

---
+++ src/main/java/pl/szymonleyk/resourcestorage/ResourceStorageApplication.java
@@ -0,0 +1,13 @@
+package pl.szymonleyk.resourcestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/szymonleyk/resourcestorage/ResourceStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szymonleyk.resourcestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
